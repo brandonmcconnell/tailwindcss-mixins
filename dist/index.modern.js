ddfdef0b34e922dfa80341a8d76f8529ec2a9ab1
Brandon McConnell <brandon@dreamthinkbuild.com>
2024-04-23 01:31:16 -0700

    Improve exports

--- dist/index.modern.js
+++ dist/index.modern.js
@@ -1,2 +1,2 @@
-import i from"tailwindcss/plugin";const m={values:{DEFAULT:""}},n=i(({matchVariant:i})=>{i("mixin",(i,{modifier:m})=>`& .mixin${m?`\\/${m}`:""} { & }`,m)});module.exports=n;
+import i from"tailwindcss/plugin";const a={values:{DEFAULT:""}};var t=i(({matchVariant:i})=>{i("mixin",(i,{modifier:a})=>`& .mixin${a?`\\/${a}`:""} { & }`,a)});export{t as default};
 //# sourceMappingURL=index.modern.js.map
--- dist/index.module.js
+++ dist/index.module.js
@@ -1,2 +1,2 @@
-import i from"tailwindcss/plugin";var n={values:{DEFAULT:""}},r=i(function(i){(0,i.matchVariant)("mixin",function(i,n){var r=n.modifier;return"& .mixin"+(r?"\\/"+r:"")+" { & }"},n)});module.exports=r;
+import i from"tailwindcss/plugin";var n={values:{DEFAULT:""}},a=i(function(i){(0,i.matchVariant)("mixin",function(i,n){var a=n.modifier;return"& .mixin"+(a?"\\/"+a:"")+" { & }"},n)});export{a as default};
 //# sourceMappingURL=index.module.js.map
